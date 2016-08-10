343ba888c4cf5d6557ede52e0fc3625630a04720
Josh Wymer <wymetyme@gmail.com>
2016-08-10 18:24:50 -0500

    Update src-index.js

--- src-index.js
+++ src-index.js
@@ -4,7 +4,8 @@ window.addEventListener('WebComponentsReady', function() {
   var CustomElement = Object.create(HTMLElement.prototype);
 
   CustomElement.attachedCallback = function() {
-    // Adding and removing a second handler will also remove the initial handler if
+    // In firefox (using the webcomponent.js polyfill), 
+    // adding and removing a second handler will also remove the initial handler if
     // the initial handler is an arrow function and useCapture: true
 
     // if you change this to either not be an arrow function OR set useCapture to false, the bug goes away.
